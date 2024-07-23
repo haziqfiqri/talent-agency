1ba5d49882e7cf81c07ca3ad0894d3f1deddb874
Haziq <fiqri.haziq@gmail.com>
2024-07-24 06:18:17 +0800

    more columns

--- src/components/app/discover.tsx
+++ src/components/app/discover.tsx
@@ -12,12 +12,12 @@ export default function Discover() {
           <div className="grid grid-cols-3 gap-3 relative">
             <img
               className="w-full h-full object-cover rounded-xl"
-              src="./model/model1.png"
+              src="./model/model4.png"
               alt=""
             />
             <img
               className="w-full h-full object-cover rounded-xl"
-              src="./model/model4.png"
+              src="./model/model1.png"
               alt=""
             />
             <img
@@ -40,8 +40,23 @@ export default function Discover() {
               src="./model/model6.png"
               alt=""
             />
+            <img
+              className="w-full h-full object-cover rounded-xl"
+              src="./model/model7.png"
+              alt=""
+            />
+            <img
+              className="w-full h-full object-cover rounded-xl"
+              src="./model/model9.png"
+              alt=""
+            />
+            <img
+              className="w-full h-full object-cover rounded-xl"
+              src="./model/model8.png"
+              alt=""
+            />
           </div>
-          <div className="absolute bottom-0 left-0 right-0 h-1/3 bg-gradient-to-t from-white via-white/80 to-transparent backdrop-blur-sm flex items-center justify-center backdrop-opacity-5">
+          <div className="absolute bottom-0 left-0 right-0 h-2/3 bg-gradient-to-t from-white via-white/80 to-transparent backdrop-blur-sm flex items-center justify-center backdrop-opacity-5">
             <button className="bg-black text-white py-2.5 px-6  rounded-xl transition duration-300">
               Explore Our Talents
             </button>
