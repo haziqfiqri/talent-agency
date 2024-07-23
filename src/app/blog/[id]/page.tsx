ba90bfe10acfcc4b3797fa5ab3ea781a3e0d032f
Haziq <fiqri.haziq@gmail.com>
2024-07-24 05:52:55 +0800

    edge function fix

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+export const runtime = "edge";
+
 export default function Page() {
   return <React.Fragment>/blog/id</React.Fragment>;
 }
--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+export const runtime = "edge";
+
 export default function Page() {
   return <React.Fragment>/project/id/</React.Fragment>;
 }
